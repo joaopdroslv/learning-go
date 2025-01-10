7dcfa68e73bb240efc297f7d1980741a7bfab894
João Pedro <pdrojoao.slv@gmail.com>
2025-01-09 22:15:33 -0300

    hello world

---
+++ hello-world/main.go
@@ -0,0 +1,11 @@
+package main // Defines this as the main package, the starting point of the program
+
+import (
+	"fmt" // Imports the fmt package for formatted I/O operations
+)
+
+// Main function, the entry point of the program
+func main() {  
+	// Using the format package.function
+	fmt.Println("Hello, World!")  // Prints a line of text to the console
+}
