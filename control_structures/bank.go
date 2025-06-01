280d3e38d1773a8beecafa88643412f1c205e6c4
JOÃO PEDRO <joao.pedro@bevioficial.com.br>
2025-06-01 03:04:51 -0300

    more concepts

---
+++ control_structures/bank.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const accountBalanceFile = "balance.txt"
+
+func getBalanceFromFile() (float64, error) {
+	data, err := os.ReadFile(accountBalanceFile) // Ignoring the error
+
+	if err != nil {
+		return 0, errors.New("failed to find balance file")
+	}
+
+	balanceText := string(data)
+	balance, err := strconv.ParseFloat(balanceText, 64)
+
+	if err != nil {
+		return 0, errors.New("failed to parse stored balance value")
+	}
+
+	return balance, nil
+}
+
+func writeBalanceToFile(balance float64) {
+	balanceText := fmt.Sprint(balance)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
+}
+
+func main() {
+	var accountBalance, err = getBalanceFromFile()
+
+	if err != nil {
+		fmt.Println("[ERROR] Something went wrong!")
+		fmt.Println(err)
+		fmt.Println("----------")
+		// panic("Can't continue without balance information.")
+	}
+
+	fmt.Println("Welcome to Go Bank!")
+
+	for {
+		fmt.Println("[MENU] What do you want to do?")
+		fmt.Println("1. Check your balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		// wantsCheckBalance := choice == 1
+
+		switch choice {
+		case 1:
+			fmt.Println("Your balance is", accountBalance)
+
+		case 2:
+			fmt.Print("Deposit amount: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			accountBalance += depositAmount
+
+		case 3:
+			fmt.Print("Withdraw amount: ")
+			var withdrawAmount float64
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("Invalid amount. Must be greater than 0.")
+				continue
+			}
+
+			if withdrawAmount > accountBalance {
+				fmt.Println("Invalid amount. Insufficient founds!")
+				continue
+			}
+
+			accountBalance -= withdrawAmount
+
+		default:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank!")
+			return
+		}
+
+		if choice == 2 || choice == 3 {
+			fmt.Println("Balance updated! New balance:", accountBalance)
+			writeBalanceToFile(accountBalance)
+		}
+	}
+}
